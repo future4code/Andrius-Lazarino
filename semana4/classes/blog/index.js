979b24a42b19fd71daf0038068ee008ce1ebeb62
Andrius <andrius.rochalazarino@gmail.com>
2020-02-05 14:00:56 -0400

    Passo 1 DESAFIO

--- semana4/classes/blog/index.js
+++ semana4/classes/blog/index.js
@@ -1,9 +1,10 @@
 
 class Post {
-    constructor(titulo, autor, conteudo){
+    constructor(titulo, autor, conteudo, imagem) {
         this.titulo = titulo
         this.autor = autor
         this.conteudo = conteudo
+        this.imagem = imagem
     }
 }
 let post = []
@@ -13,19 +14,30 @@ function criar() {
     const titulo = document.getElementById("titulo")
     const autor = document.getElementById("autor")
     const conteudo = document.getElementById("conteudo")
-    post.push(new Post(titulo.value, autor.value, conteudo.value))
-    //console.log(post)
-    inserirPost(post)
+    const imagem = document.getElementById("imagem")
+    post.push(new Post(titulo.value, autor.value, conteudo.value, imagem.value))
+    console.log(post)
+    console.log(post.length)
+    console.log(post[1])
+    if ((titulo.value === "") || (autor.value === "") || (conteudo.value === "")) {
+        alert("Título, autor e conteúdo são campos obrigatórios!")
+    }
+    else {
+        inserirPost(post)
+    }
     titulo.value = ""
     autor.value = ""
     conteudo.value = ""
+    imagem.value = ""
 }
-console.log("DEBUG: "+document.getElementsByTagName("section"))
-console.log(document.getElementsByTagName("section")[0])
-
 function inserirPost(post) {
+    const tamanho = post.length-1
     let postSection = document.getElementsByTagName("section")
-    console.log("debugs3: "+ post[length].titulo)
-    postSection[0].innerHTML += "<p>" +"<h3>"+ post[length].titulo +"</h3>"+ post[length].conteudo + "<p>Escrito por: "+ post[length].autor+"</p>" + "</p>"
-    
+    if (post[tamanho].imagem === "") {
+        console.log("entrou aqui")
+        postSection[0].innerHTML += "<p>" + "<h3>" + post[tamanho].titulo + "</h3>" + "<p class='texto'>" + post[tamanho].conteudo + "</p>" + "<p>Escrito por: " + post[tamanho].autor + "</p>" + "</p>"
+    }
+    else {
+        postSection[0].innerHTML += "<p>" + "<h3>" + post[tamanho].titulo + "</h3>" + "<p class='texto'>" + post[tamanho].conteudo + "</p>" + "<p><img src='" + post[tamanho].imagem + "'></p>" + "<p>Escrito por: " + post[tamanho].autor + "</p>" + "</p>"
+    }
 }
