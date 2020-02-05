c799cbad8aaea7f0c6a88231cb9ecdfe278869be
Andrius <andrius.rochalazarino@gmail.com>
2020-02-05 12:57:33 -0400

    Passo 5

--- semana4/classes/blog/index.js
+++ semana4/classes/blog/index.js
@@ -7,13 +7,25 @@ class Post {
     }
 }
 let post = []
+
+
 function criar() {
     const titulo = document.getElementById("titulo")
     const autor = document.getElementById("autor")
     const conteudo = document.getElementById("conteudo")
     post.push(new Post(titulo.value, autor.value, conteudo.value))
-    console.log(post)
+    //console.log(post)
+    inserirPost(post)
     titulo.value = ""
     autor.value = ""
     conteudo.value = ""
+}
+console.log("DEBUG: "+document.getElementsByTagName("section"))
+console.log(document.getElementsByTagName("section")[0])
+
+function inserirPost(post) {
+    let postSection = document.getElementsByTagName("section")
+    console.log("debugs3: "+ post[length].titulo)
+    postSection[0].innerHTML += "<p>" +"<h3>"+ post[length].titulo +"</h3>"+ post[length].conteudo + "<p>Escrito por: "+ post[length].autor+"</p>" + "</p>"
+    
 }
