fe7e505df444bb9bf12785ac66d1b34bf3d5db7e
Andrius <andrius.rochalazarino@gmail.com>
2020-02-07 10:48:30 -0400

    Exercícios de interpretação de código EXERCICIO 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+//Exercícios de interpretação de código ---------------------------------------------------------------------------------------------------
+
+// 1. conversorDeMoeda(valorEmDolar) recebe como argumento um valor para converter o dollar em reais, na função ele le do usuário o valor
+//da cotação que retorna multiplicado pelo valor do argumento
+
