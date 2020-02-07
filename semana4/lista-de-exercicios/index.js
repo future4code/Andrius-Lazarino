b735a0be09a71904962670e56587b51bfcde9ed2
Andrius <andrius.rochalazarino@gmail.com>
2020-02-07 10:49:42 -0400

    Exercícios de interpretação de código EXERCICIO 3

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,3 +6,6 @@
 // 2. inverteDinheiro("Tesouro Direto", 200) recebe como argumento o tipo de investimento e o valor, dentro da função um switch case calcula com base
 //no tipo de investimento. Valor impresso no console: 165; TIPO DE INVESTIMENTO INFOMADO INCORRETO!
 
+// 3. Guarda os numeros pares e impares de uma váriavel em váriaveis diferentes, mas existe um problema as arrays estão declaradas como const oque vai resultar
+//em erro
+
