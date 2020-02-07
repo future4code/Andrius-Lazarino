1fbf120d231e6fcca91157539c0ed3ea2db3cf52
Andrius <andrius.rochalazarino@gmail.com>
2020-02-07 10:49:14 -0400

    Exercícios de interpretação de código EXERCICIO 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,6 @@
 // 1. conversorDeMoeda(valorEmDolar) recebe como argumento um valor para converter o dollar em reais, na função ele le do usuário o valor
 //da cotação que retorna multiplicado pelo valor do argumento
 
+// 2. inverteDinheiro("Tesouro Direto", 200) recebe como argumento o tipo de investimento e o valor, dentro da função um switch case calcula com base
+//no tipo de investimento. Valor impresso no console: 165; TIPO DE INVESTIMENTO INFOMADO INCORRETO!
+
