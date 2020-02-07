4f32df1a078fda797c6d323cc7b54399f1679927
Andrius <andrius.rochalazarino@gmail.com>
2020-02-07 10:51:41 -0400

    Exercícios de lógica de programação EXERCICIO 1

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -11,3 +11,16 @@
 
 // 4. Compara os numeros para ver quem é o maior e menor da lista, valores impressos no console: -10; 1590
 
+//Exercícios de lógica de Programação ------------------------------------------------------------------------------------------------------
+
+// 1. "forEach" utilizado para percorrer a array para mostrar seus valores ou modificalos, com "map" criamos uma nova array com o mesmo tamanho mas
+//com as propriedades selecionadas, com "filter" criamos um novo array de tamanho menor ou igual com elementos filtrados.
+//logicaProgramaçãoExercicio1()
+
+
+function logicaProgramaçãoExercicio1() {
+    const array = [1, 5, 15, 45, 64, 78, 98, 154, 168]
+    array.forEach((numero, index, array) => {
+        console.log("Numero: " + numero)
+    });
+}
