ea63f1bf72c5d1b6370c6906a54a329170bd81e7
Andrius <andrius.rochalazarino@gmail.com>
2020-02-11 21:36:32 -0400

    Adicionado efeito no Ver Mais

--- semana5/perfil-profissional/src/Components/ImageButton/ImageButton.js
+++ semana5/perfil-profissional/src/Components/ImageButton/ImageButton.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './ImageButton.css';
 
+ let pageSectionLista = document.getElementsByClassName("PageSection")
+
 function ImageButton(props) {
+     function verMaisMenos() {      
+        
+        if (props.descricao === "Ver Mais"){
+            pageSectionLista[0].style.display = "none";
+            pageSectionLista[1].style.display = "none";
+            pageSectionLista[2].style.display = "none";
+            pageSectionLista[3].style.display = "grid";
+        }
+        else{
+            pageSectionLista[0].style.display = "grid";
+            pageSectionLista[1].style.display = "grid";
+            pageSectionLista[2].style.display = "grid";
+            pageSectionLista[3].style.display = "none";
+        }
+    }
     return (
-        <div className="ImageButton">
+        <div className="ImageButton" onClick={verMaisMenos}>
             <a href={props.redirecionar} target="_blank">
                 <img className="ImagemButton" src={props.imagem} />
                 <p>{props.descricao}</p>
