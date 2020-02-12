28f314723c1258ee575753f7d9596de2ef06a028
Andrius <andrius.rochalazarino@gmail.com>
2020-02-12 15:25:25 -0400

    atividade aula19

---
+++ semana5/feed-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Post from './Components/Post/Post';
+
+function App() {
+  return (
+    <div className="App">
+      <Post nome="Andrius" imagem={require('./img/imagem1.jpg')} imagemUsuario={require('./img/perfil.jpg')} />
+      <Post nome="Andrius" imagem={require('./img/imagem2.jpeg')} imagemUsuario={require('./img/perfil.jpg')} />
+      <Post nome="Andrius" imagem={require('./img/imagem3.jpg')} imagemUsuario={require('./img/perfil.jpg')} />
+    </div>
+  );
+}
+
+export default App;
