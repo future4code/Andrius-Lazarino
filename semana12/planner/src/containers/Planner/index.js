bf6a03a030f00afe576202aa74711b6788590d13
Andrius <andrius.rochalazarino@gmail.com>
2020-04-11 11:35:53 -0400

    Adicionado dias em um form

--- semana12/planner/src/containers/Planner/index.js
+++ semana12/planner/src/containers/Planner/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 
-const PlannerWrapper = styled.div`
+const PlannerWrapper = styled.form`
   width: 800px;
   display: flex;
   flex-direction: column;
@@ -22,33 +22,75 @@ const Day = styled.div`
   width: 266px;
 `
 
+const plannerDaysForm = [
+  {
+    name: "dia",
+    label: "Segunda",
+    type: "radio",
+    value: "segunda",
+    title: "Segunda"
+  },
+  {
+    name: "dia",
+    label: "Terça",
+    type: "radio",
+    value: "terca",
+    title: "Terça"
+  },
+  {
+    name: "dia",
+    label: "Quarta",
+    type: "radio",
+    value: "quarta",
+    title: "Quarta"
+  },
+  {
+    name: "dia",
+    label: "Quinta",
+    type: "radio",
+    value: "quinta",
+    title: "Quinta"
+  },
+  {
+    name: "dia",
+    label: "Sexta",
+    type: "radio",
+    value: "sexta",
+    title: "Sexta"
+  },
+  {
+    name: "dia",
+    label: "Sabado",
+    type: "radio",
+    value: "sabado",
+    title: "Sabado"
+  },
+  {
+    name: "dia",
+    label: "Domingo",
+    type: "radio",
+    value: "domingo",
+    title: "Domingo"
+  }
+  
+]
+
 class Planner extends React.Component {
   render() {
     return <PlannerWrapper>
       <h2>Planner</h2>  
         <input />
         <DaysSelect>
-          <div>
-            <input type="radio" name="dia" value="segunda" />SEGUNDA
-          </div>
-          <div>
-            <input type="radio" name="dia" value="terca" />TERÇA
-          </div>
-          <div>
-            <input type="radio" name="dia" value="quarta" />QUARTA
-          </div>
-          <div>
-            <input type="radio" name="dia" value="quinta" />QUINTA
-          </div>
-          <div>
-            <input type="radio" name="dia" value="sexta" />SEXTA
-          </div>
-          <div>
-            <input type="radio" name="dia" value="sabado" />SÁBADO
-          </div>
-          <div>
-            <input type="radio" name="dia" value="domingo" />DOMINGO
-          </div>
+        {
+            plannerDaysForm.map((form)=>{
+              return (
+                <div key={form.value}>
+                  {form.label}
+                  <input type={form.type} name={form.name} value={form.value} onClick={()=>{console.log("Oi")}} />
+                </div>
+              )
+            })
+          }
         </DaysSelect>
         <button>Criar Tarefa</button>
         <Days>
