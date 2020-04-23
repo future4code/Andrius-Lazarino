6610ba02a39b0882f794b5586545b7abbcddadd6
Andrius <andrius.rochalazarino@gmail.com>
2020-04-23 16:25:16 -0400

    Arquivo lido com sucesso

--- semana14/promises/src/exercicio1.ts
+++ semana14/promises/src/exercicio1.ts
@@ -1,7 +1,42 @@
 import {readFile} from "fs"
+//
+// const myPromise = new Promise((resolve,reject)=>{
+//     const handleFileRead = (err: Error, data: Buffer)=>{
+//         try {
+//             const fileContent: string = data.toString();
+//             resolve(fileContent)
+//         } catch(err){
+//             reject(err)
+//         }
+//     }
+//     readFile("1.txt", handleFileRead);
+// })
+//
+// myPromise.then((result: string)=>{
+//     console.log(result)
+// }).catch((err)=>{
+//     console.error(err);
+// })
 
-const myPromise = new Promise((resolve,reject)=>{
-    try {
-        
+
+const myPromise = new Promise((resolve, reject) => {
+
+    const handleFileRead = (err: Error, data: Buffer) => {
+        try {
+            const fileContent: string = data.toString();
+            resolve(fileContent)
+        } catch (e) {
+            reject(err)
+        }
     }
+    readFile("./src/1.txt", handleFileRead);
 })
+
+myPromise
+    .then((result: string)=>{
+        //faz o que você quiser aqui porque deu certo
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
