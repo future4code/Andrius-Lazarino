c40bb581e6aec7e29192fb212c307ef92e57498c
Andrius <andrius.rochalazarino@gmail.com>
2020-04-29 14:48:11 -0400

    Exercicio 3 completo

--- semana15/polimorfismo/src/index.ts
+++ semana15/polimorfismo/src/index.ts
@@ -3,11 +3,17 @@
 
 // Exercício 2
 // A. Cannot create an instance of an abstract class.
-// B. Precisa criar uma outra classe que se estende da classe abstrata para usala
+// B. Precisa criar uma outra classe que se estende da classe abstrata para usá-la
 
+// Exercício 3
+// A. Precisa criar uma outra classe que se estende da classe abstrata para usá-la.
+// B. Porque possui uma função que retorna um valor e em uma interface tudo é abstrato.
+// C. Porque ela é uma classe que serve de modelo ou base para outras classes e sozinha não consegue fazer quase nada.
 
 import {Client} from "./Client"
-import {Place} from "./Place"
+import {Residence} from "./Residence";
+import {Commerce} from "./Commerce";
+import {Industry} from "./Industry";
 
 const objeto:Client = {
     name: "Andrius",
@@ -22,3 +28,12 @@ const objeto:Client = {
 // console.log(objeto.registrationNumber)
 // console.log(objeto.consumedEnergy)
 // console.log(objeto.calculateBill())
+
+const residence = new Residence(2,"99333")
+const commerce =  new Commerce(6,"33999")
+const industry = new Industry(9,"93399")
+
+console.log(residence.getCep())
+console.log(commerce.getCep())
+console.log(industry.getCep()
+)
